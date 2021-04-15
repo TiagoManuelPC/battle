7f0e9ba8ebe322177cc8c64531082c9c9d0164d7
Tiago <tiagomanuelpereiracampos@hotmail.com>
2021-04-15 16:50:28 +0100

    Global variable

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
 require 'sinatra'
 require 'sinatra/reloader' 
+require './lib/player'
 
 class Battle < Sinatra::Base
   
   configure :development do
     register Sinatra::Reloader
-    
   end
 enable :sessions
   get '/' do
@@ -13,20 +13,20 @@ enable :sessions
   end
   
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
